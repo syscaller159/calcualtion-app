6276d21297373af352721f13e5cbb2526162ac7e
Azizbek Allayarov <underground.x5@yandex.ru>
2021-04-08 21:11:13 +0300

    calculator-service created

--- src/calculator-module/calculator-module.module.ts
+++ src/calculator-module/calculator-module.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CalculatorModuleController } from './calculator-module.controller';
+import { CalculatorModuleService } from './calculator-module.service';
 
 @Module({
-  controllers: [CalculatorModuleController]
+  controllers: [CalculatorModuleController],
+  providers: [CalculatorModuleService]
 })
 export class CalculatorModuleModule {}
