67454931404da4439d6ada0f1edb6c7c1c0ac4e1
Azizbek Allayarov <underground.x5@yandex.ru>
2021-04-08 21:07:51 +0300

    calculator-module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CalculatorModuleModule } from './calculator-module/calculator-module.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CalculatorModuleModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
