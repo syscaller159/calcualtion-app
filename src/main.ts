fe8bf0002259257ece0af051b5ccdb2793df7e8a
Azizbek Allayarov <underground.x5@yandex.ru>
2021-04-10 11:04:57 +0300

    server config fix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverConfiguration from './config/server.config';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(serverConfiguration.port || 8000);
 }
 bootstrap();
