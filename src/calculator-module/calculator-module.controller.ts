400e857c3ad82cc8153abfe449b8ec2e93fa7cab
Azizbek Allayarov <underground.x5@yandex.ru>
2021-04-10 11:47:10 +0300

    api controller configured

--- src/calculator-module/calculator-module.controller.ts
+++ src/calculator-module/calculator-module.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
+import { CalculateParamsDto } from './dto/calculate-params.dto';
+import { CalculatorModuleService } from './calculator-module.service';
 
-@Controller('calculator-module')
-export class CalculatorModuleController {}
+@Controller('calculator')
+export class CalculatorModuleController {
+
+  constructor(
+    private readonly calculatorModuleService: CalculatorModuleService,
+  ) {}
+
+  @Post()
+  async calculate(@Query() params: CalculateParamsDto): Promise<number> {
+    return this.calculatorModuleService.createCalculationProcess(params);
+  }
+
+  @Get()
+  async getCalculationJobId(@Query() params): Promise<number> {
+    return this.calculatorModuleService.getCalculationResult(params.id);
+  }
+
+  @Get('list')
+  getCalculationJobList() {
+    return this.calculatorModuleService.getCalculationList();
+  }
+}
